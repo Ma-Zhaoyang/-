9c1558f66b641464c3c002188dc60f8a1cd5e737
Ma-Zhaoyang <54953169+Ma-Zhaoyang@users.noreply.github.com>
2019-10-31 20:42:54 +0800

    实现SQL server2017的连接，并传输数据

---
+++ Xiao04/src/SQL_Link/SQLserver_Link.java
@@ -0,0 +1,74 @@
+package SQL_Link;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SQLserver_Link {
+	 public static void main(String[] args) throws SQLException {
+		 String str = "";
+		 try{  Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+				System.out.println("连接成功1");
+         }
+         catch(Exception e) {
+         		System.out.println("连接失败1");
+         }
+        String connectionUrl =
+                "jdbc:sqlserver://127.0.0.1:1433;"
+                        + "databaseName=test;"
+                        + "user=sa;"
+                        + "password=123456789";
+        Connection con = null;//处理与特定数据库的连接
+
+        try {
+        		 con = DriverManager.getConnection(connectionUrl);
+				System.out.println("连接成功2");
+				Statement stmt = con.createStatement(
+		        		ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+		        String sql = "select * from link ";
+		        ResultSet rs = stmt.executeQuery(sql);
+		        if(rs.next())
+		          str = rs.getString("password");
+		        rs.close();
+		        stmt.close();
+		        con.close();
+		        System.out.println(str);
+}
+		catch(SQLException e) {
+				System.out.println("请输入正确的表名"+e);
+				System.out.println("连接失败2");
+       }
+        
+   }
+}
+
+
+
+
+
+/*// 加载数据库驱动，注册到驱动管理器
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        // 数据库连接字符串
+        String url = "jdbc:sqlserver://127.0.0.1:1433";
+        // 数据库用户名
+        String usename = "sa";
+        // 数据库密码
+        String psw = "123456789";
+        // 创建Connection连接
+
+        Connection conn = DriverManager.getConnection(url,usename,psw);
+        // 判断数据库连接是否为空
+        if(conn != null){
+            String sql="select * from tb_user where name='"+username+"' and password='"+ password + "'";
+            Statement stmt = conn.createStatement();
+            ResultSet rs=stmt.executeQuery(sql);
+            if(rs.next()){
+                application.setAttribute("name",username);
+                String description = rs.getString("description");
+                application.setAttribute("description",description);
+                response.sendRedirect("success.jsp");
+            }else{
+                response.sendRedirect("Myjsp.jsp");
+*/
